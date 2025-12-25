f0b2cb7da5a5478b6018730706b7dbd76b198438
nashide <36805169+nashide@users.noreply.github.com>
2025-12-25 15:11:29 +0800

    Update LibraryTabId.kt

--- app/src/main/java/com/theveloper/pixelplay/data/model/LibraryTabId.kt
+++ app/src/main/java/com/theveloper/pixelplay/data/model/LibraryTabId.kt
@@ -8,12 +8,12 @@ enum class LibraryTabId(
     val title: String,
     val defaultSort: SortOption
 ) {
-    SONGS("SONGS", "SONGS", SortOption.SongTitleAZ),
-    ALBUMS("ALBUMS", "ALBUMS", SortOption.AlbumTitleAZ),
-    ARTISTS("ARTIST", "ARTIST", SortOption.ArtistNameAZ),
-    PLAYLISTS("PLAYLISTS", "PLAYLISTS", SortOption.PlaylistNameAZ),
-    FOLDERS("FOLDERS", "FOLDERS", SortOption.FolderNameAZ),
-    LIKED("LIKED", "LIKED", SortOption.LikedSongDateLiked);
+    SONGS("歌曲", "歌曲", SortOption.SongTitleAZ),
+    ALBUMS("专辑", "专辑", SortOption.AlbumTitleAZ),
+    ARTISTS("歌手", "歌手", SortOption.ArtistNameAZ),
+    PLAYLISTS("歌单", "歌单", SortOption.PlaylistNameAZ),
+    FOLDERS("文件夹", "文件夹", SortOption.FolderNameAZ),
+    LIKED("收藏", "收藏", SortOption.LikedSongDateLiked);
 
     companion object {
         fun fromStorageKey(key: String): LibraryTabId =
